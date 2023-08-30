1e7031267ca4140dcac357dcb7bc5eb325a962d2
Sajal Chandaiya <66727050+sajal0206@users.noreply.github.com>
2023-08-31 00:24:33 +0530

    Add files via upload

--- routes/hello.dart
+++ routes/hello.dart
@@ -1,5 +1,5 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Response onRequest(RequestContext context) {
-  return Response(body: 'This is a new route!');
+  return Response(body: 'New Hello Route');
 }
